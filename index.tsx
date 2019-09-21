74ee37ed88adc2edbaac31e8bdaa1df1ce5d88f9
Walter <relw1wa-github@yahoo.de>
2019-09-21 18:16:55 +0200

    Use typescript and integrate react intl

---
+++ index.tsx
@@ -0,0 +1,33 @@
+import es6Promise from 'es6-promise';
+es6Promise.polyfill();
+
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
+
+import { messages } from './messages';
+
+import '../config/font-awesome';
+import '../styles/global-styles.scss';
+
+import Portfolio from './components/portfolio';
+import ScrollToTop from '../reusable-components/scroll-to-top';
+
+let locale = navigator ? navigator['language'].substr(0,2) : 'en';
+locale = messages.hasOwnProperty(locale) ? locale : 'en';
+
+hydrate(
+  <IntlProvider
+    defaultLocale="en"
+    locale={locale}
+    messages={messages[locale]}
+  >
+    <BrowserRouter>
+      <ScrollToTop>
+        <Portfolio />
+      </ScrollToTop>
+    </BrowserRouter>
+  </IntlProvider>,
+  document.getElementById('root')
+);
---
+++ messages.ts
@@ -0,0 +1,78 @@
+export const messages: Object = {
+  en: {
+    'markdownPreviewer.ttl.global.appTitle': 'Markdown Previewer',
+    'markdownPreviewer.ttl.global.markdownInput': 'Markdown Input',
+    'markdownPreviewer.ttl.global.htmlOutput': 'HTML Output',
+    'markdownPreviewer.btn.controls.reset': 'Reset',
+    'markdownPreviewer.btn.controls.instructions': 'Instructions',
+    'markdownPreviewer.btn.controls.hideInstructions': 'Hide Instructions',
+    'markdownPreviewer.btn.controls.emphasis': 'Emphasis',
+    'markdownPreviewer.btn.controls.headings': 'Headings',
+    'markdownPreviewer.btn.controls.paragraphs': 'Paragraphs',
+    'markdownPreviewer.btn.controls.links': 'Links',
+    'markdownPreviewer.btn.controls.lists': 'Lists',
+    'markdownPreviewer.txt.inputArea.introText': 'Enter **Github**-*flavored* ***Markdown*** and see it transform to \`HTML\`',
+    'markdownPreviewer.txt.inputArea.placeholder': 'Input Github-flavored Markdown',
+    'markdownPreviewer.txt.inputArea.emphasis': 'You can emphasize text like this:\n\n' +
+    '*simple emphasis*\n' +  
+    '**strong emphasis**\n' +  
+    '***combined emphasis***\n' +
+    '~~strikethrough~~',
+    'markdownPreviewer.txt.inputArea.headings': 'There are headings:\n' +
+    '# Level 1\n' +
+    '## Level 2\n' +
+    'and so on til\n' +
+    '###### Level 6\n',
+    'markdownPreviewer.txt.inputArea.paragraphs': 'You get a new line with two spaces at the end  \n' +
+    'And a paragraph by leaving an empty line:\n\n' +    
+    'Like this.',
+    'markdownPreviewer.txt.inputArea.links': 'You can create relative references like this [description](#).\n\n' +
+    'URLs get turned into links automatically: https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet',    
+    'markdownPreviewer.txt.inputArea.lists': 'There are ordered lists like this:\n' +
+    '1. Item one\n' +
+    '2. Item two\n' +
+    '3. Item three\n\n' +
+    'And there are unordered lists:\n' +
+    '* A Item\n' +
+    '+ Another Item\n' +
+    '- Yet another Item',
+  },
+  de: {
+    'markdownPreviewer.ttl.global.appTitle': 'Markdown Previewer',
+    'markdownPreviewer.ttl.global.markdownInput': 'Eingabe von Markdown',
+    'markdownPreviewer.ttl.global.htmlOutput': 'Ausgabe von HTML',
+    'markdownPreviewer.btn.controls.reset': 'Zurücksetzen',
+    'markdownPreviewer.btn.controls.instructions': 'Anweisungen',
+    'markdownPreviewer.btn.controls.hideInstructions': 'Anweisungen verbergen',
+    'markdownPreviewer.btn.controls.emphasis': 'Hervorhebung',
+    'markdownPreviewer.btn.controls.headings': 'Überschriften',
+    'markdownPreviewer.btn.controls.paragraphs': 'Absätze',
+    'markdownPreviewer.btn.controls.links': 'Links',
+    'markdownPreviewer.btn.controls.lists': 'Listen',
+    'markdownPreviewer.txt.inputArea.introText': 'Gib **Github**-*angereichertes* ***Markdown*** ein und erlebe, wie es zu \`HTML\` wird',
+    'markdownPreviewer.txt.inputArea.placeholder': 'Gib Github-angereichertes Markdown ein',
+    'markdownPreviewer.txt.inputArea.emphasis': 'Du kannst Text folgendermaßen hervorheben:\n\n' +    
+    '*einfache Hervorhebung*\n' +
+    '**starke Hervorhebung**\n' +  
+    '***kombinierte Hervorhebung***\n' +
+    '~~durchgestrichen~~',
+    'markdownPreviewer.txt.inputArea.headings': 'Es gibt Überschriften:\n' +
+    '# Ebene 1\n' +
+    '## Ebene 2\n' +
+    'und so weiter bis\n' +
+    '###### Ebene 6\n',
+    'markdownPreviewer.txt.inputArea.paragraphs': 'Du bekommst eine neue Zeile mit zwei Leerzeichen am Ende  \n' +
+    'Und einen Absatz, indem eine leere Zeile gelassen wird:\n\n' +    
+    'So wie hier.',
+    'markdownPreviewer.txt.inputArea.links': 'So kannst du relative Referenzen erstellen: [Beschreibung](#).\n\n' +
+    'URLs werden automatisch in Links verwandelt: https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet',    
+    'markdownPreviewer.txt.inputArea.lists': 'Es gibt geordnete Listen:\n' +
+    '1. Erster Eintrag\n' +
+    '2. Zweiter Eintrag\n' +
+    '3. Dritter Eintrag\n\n' +
+    'Und es gibt ungeordnete Listen:\n' +
+    '* Ein Eintrag\n' +
+    '+ Noch ein Eintrag\n' +
+    '- Und noch ein weiterer Eintrag',
+  },
+};
